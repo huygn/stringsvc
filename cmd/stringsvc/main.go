7dfcc25b9c46b3c7f198e53ba2b2e7244af3ddd1
Huy Giang <gnhuy91@gmail.com>
2016-08-17 10:59:10 +0700

    use NewSyncWriter for safely log writes

--- cmd/stringsvc/main.go
+++ cmd/stringsvc/main.go
@@ -15,7 +15,7 @@ func main() {
 
 	var logger log.Logger
 	{
-		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
 		logger = log.NewContext(logger).With("ts", log.DefaultTimestampUTC)
 		logger = log.NewContext(logger).With("caller", log.DefaultCaller)
 	}
